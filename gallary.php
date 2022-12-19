cb3f130284e01cb5b873f57d9532596b1fb9a12e
bahaa <blainefire00@gmail.com>
2022-12-19 23:47:03 +0300

    done gallery part

--- gallary.php
+++
@@ -1,9 +0,0 @@
-<?php
-$numberOfCards = 10; // this is the number of images you want to show
-for ($i=0; $i <= $numberOfCards ; $i++) { 
-    $card = $cards[$i];
-    echo "<img src='img/gallery/$i.jpg'> <br>";
-    }
-
-
-?>
