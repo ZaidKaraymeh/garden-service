e9885905aec88f47a3d2b92b1ebc74e8eabf3e87
bahaa <blainefire00@gmail.com>
2022-12-28 23:47:04 +0300

    cart details

--- cart.php
+++ cart.php
@@ -33,23 +33,243 @@ Established in 2016, we provide unique garden solutions, Land services, Landscap
 <body>
     <?php require('includes/user_header.php'); ?>
     <!--Container Main start-->
-    <div class="cart-container">
-        <h4>Main Components</h4>
-    </div>
-    <!--Container Main end-->
+    <div class="container ">
+        <div class="row">
+            <div class="col-12 col-md-6 pic">
+                <!--activite pic-->
+                <div class="title "><?php echo $name; ?></div>
+                <img src="Images\<?php echo $photo; ?>" />
 
-    <!-- Footer Section -->
-    <div id="footer">
-        <div class="container text-center">
-            <div class="col-md-12 col-md-offset-2">
-                <p>CopyRights <i class="far fa-copyright"></i>2022 Bahrain Branch Contracting <i
-                        class="fas fa-trademark"></i>
-                </p>
             </div>
+            <div class="col-12 col-md-6 info">
+                <!--booking-->
+                <div class="container">
+                    <div class="panel panel-primary">
+
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <details>
+                                        <summary>Information</summary>
+                                        <ul>
+                                            <li>
+                                                <p>price per person.</p>
+                                            </li>
+                                            <li>
+                                                <p>minimum 1 hour.</p>
+                                            </li>
+                                            <li>
+                                                <p>Life jacket will be provided for.</p>
+                                            </li>
+
+                                        </ul>
+                                    </details>
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+                            <!--row1-->
+                            <div class="row">
+                                <!--col1-->
+                                <div class="col-md-6  ">
+                                    <form method="POST" action="reservation.php">
+                                        <div class="form-group">
+                                            <label class="control-label">DATE</label>
+                                            <input type="date" class="form-control" name="date" id="date"
+                                                max=2030-12-30>
+                                        </div>
+                                </div><!--col1-->
+                                <!--col2-->
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="control-label">TIME</label><br>
+                                        <select class="form-select" aria-label=".form-select-lg example"
+                                            style="max-width:400px; padding-top:7px; padding-bottom:7px;" name="time">
+                                            <option selected>select time</option>
+                                            <option value="6:00am-7:00am">6:00am-7:00am</option>
+                                            <option value="7:00am-8:00am">7:00am-8:00am</option>
+                                            <option value="8:00am-9:00am">8:00am-9:00am</option>
+                                            <option value="9:00am-10:00am">9:00am-10:00am</option>
+                                            <option value="10:00am-11:00am">10:00am-11:00am</option>
+                                            <option value="11:00am-12:00pm">11:00am-12:00pm</option>
+                                            <option value="1:00pm-2:00pm">1:00pm-2:00pm</option>
+                                            <option value="2:00pm-3:00pm">2:00pm-3:00pm</option>
+                                            <option value="3:00pm-4:00pm">3:00pm-4:00pm</option>
+                                            <option value="4:00pm-5:00pm">4:00pm-5:00pm</option>
+                                            <option value="5:00pm-6:00pm">5:00pm-6:00pm</option>
+
+
+
+                                        </select>
+                                    </div>
+                                </div>
+                            </div><!--col2-->
+                            <!--row2-->
+                            <div class="row">
+                                <?php if (!$v) { //water activity ?>
+
+                                    <!--col1-->
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label">PERSONS</label>
+                                            <input type="number" class="form-control" name="persons" id="persons" min=1
+                                                max=5>
+                                        </div>
+                                    </div><!--col1-->
+                                    <?php } else { ?> <input type='hidden' name='persons' value='1' />
+                                    <?php } ?>
+                                <!--col2-->
+                                <div class='col-md-6'>
+                                    <div class="form-group">
+                                        <label class="control-label">RATE</label>
+                                        <div class='input-group date' id='rate'>
+                                            <?php
+                                            if ($t) {
+                                                for ($i = 1; $i <= $avgrate; $i++) { ?>
+                                                    <i class="fa-solid fa-star"></i>
+                                                    <?php }
+                                            } else
+                                                echo "no rate yet"; ?>
+                                            <span class="input-group-addon">
+                                                <span class="glyphicon glyphicon-calendar"></span>
+                                            </span>
+                                        </div>
+                                    </div><!--col2-->
+                                </div><!--row1-->
+                                <!--row2-->
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label class="control-label">PRICE</label><br>
+                                            <label class="control-label">
+                                                <?php echo $price; ?> BD PER HOUR
+                                            </label>
+                                        </div>
+                                    </div><!--row2-->
+                                </div>
+                            </div>
+                            <?php if (!$v) { ?>
+                                <?php } ?>
+                            <input type='hidden' name='aid' value='<?php echo $aid; ?>' />
+                            <input type='hidden' name='price' value='<?php echo $price; ?>' />
+                            <input type="submit" name="btn" class="btn btn-info btn-lg btn-block" value="BOOK NOW">
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div><!--row1-->
+
+            <?php
+            # php code 
+            
+            //select usename -->> $uid=$row['uid']
+//hidden submit value=aid
+//insert date,time,no.persons,uid,aid
+            $aid = $_REQUEST['aid'];
+            try {
+                require('connection.php');
+                $rs = $db->query("SELECT * FROM activity  WHERE id = $aid ");
+                $r = $db->query("SELECT * FROM comments  WHERE aid = $aid");
+                while ($row = $rs->fetch()) {
+                    $name = $row['name'];
+                    $price = $row['price'];
+                    $photo = $row['photo'];
+                    $type = $row['type'];
+                }
+                $comment = array();
+                if ($r->rowCount() > 0) {
+                    while ($rr = $r->fetch()) {
+                        $comment[] = $rr['comment'];
+                        $rate[] = $rr['rate'];
+                        $un[] = $rr['username'];
+                    }
+                }
+                $v = false;
+                if ($type == 'land') {
+                    $v = true;
+                }
+                $db = null;
+            } catch (PDOException $e) {
+                echo "error occured!";
+                die($e->getMessage());
+            }
+
+            //calc the avarage rate
+            $sumrate = 0;
+            $count = 0;
+
+
+            if ($r->rowCount() > 0) {
+                if (!is_null($rate)) {
+                    foreach ($rate as $value) {
+                        if (!is_null($value)) {
+                            $sumavg += $value;
+                            $count++;
+                        }
+                    }
+                }
+            }
+            $t = true;
+            if ($sumrate == 0 && $count == 0) {
+                $t = false;
+            }
+            if ($t) {
+                $avgrate = $sumavg / $count;
+            }
+
+            ?>
+            <!--comments-->
+            <?php if (!empty($comment)) { ?>
+                <div class="row ">
+                    <?php foreach ($comment as $k => $v) {
+                    if (!is_null($v)) { ?>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <?php echo $un[$k]; ?>
+                                        </h5>
+                                        <p class="card-text"><?php echo $v; ?></p>
+                                        <p class="card-text"><small class="text-muted">
+                                                <?php if (!is_null($rate[$k])) {
+                                                echo 'rate ' . $rate[$k] . '/5';
+                                            } ?>
+                                            </small></p>
+                                    </div>
+                                </div>
+                            </div><!--col1-->
+
+                            <?php }
+                }
+                            } ?>
+
+            </div><!--row2-->
+        </div><!--container-->
+</body>
+
+</html>
+<!--Container Main end-->
+
+<!-- Footer Section -->
+<div id="footer">
+    <div class="container text-center">
+        <div class="col-md-12 col-md-offset-2">
+            <p>CopyRights <i class="far fa-copyright"></i>2022 Bahrain Branch Contracting <i
+                    class="fas fa-trademark"></i>
+            </p>
         </div>
     </div>
-    <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
 
 </html>
--- viewreservations.php
+++ viewreservations.php
@@ -1,242 +1,229 @@
 <?php
 session_start();
-require('authorization.php');
-if(isset($_SESSION['admin'])){
-  require('adminheader.php');
-  $username=$_SESSION['admin'];
-}else
-{  require('header2.php');
-  $username=$_SESSION['activeuser'];
+
+
+
+
+
+
+//view all reservation
+//add comment+rate
+
+date_default_timezone_set("Asia/Bahrain"); //change server time to bahrain time
+try {
+  $rs = $db->query("select * from user where username ='$user' ");
+  $s = $db->query("SELECT * FROM comments  WHERE username = '$user' ");
+
+  while ($rr = $rs->fetch()) {
+    $uid = $rr['id'];
+  }
+  $r = $db->query("select * from reservation where uid= $uid");
+  $c = 0;
+  while ($row = $r->fetch()) {
+    $aid[] = $row['aid'];
+    $date[] = $row['date'];
+    $time[] = $row['time'];
+    $aname[] = $row['aName'];
+    $rid[] = $row['rid'];
+    $price[] = $row['price'];
+    ++$c;
+  }
+  while ($ss = $s->fetch()) {
+    $caid[] = $ss['aid'];
+    $crid[] = $ss['rid'];
+    if (!is_null($ss['rate'])) {
+      $crate[] = 100; //if null add 100 if not add the rate value which is between  1 to 5
+    } else
+      $crate[] = $ss['rate'];
+  }
+} catch (PDOException $ex) {
+  echo "Error occured";
+  die($ex->getMessage());
+
 }
-
-
-
-
-
-        //view all reservation
-        //add comment+rate
-
-        date_default_timezone_set("Asia/Bahrain"); //change server time to bahrain time
-      try{
-          require('connection.php');
-          $rs = $db->query("select * from users where username ='$user' ");
-          $s=$db->query("SELECT * FROM comments  WHERE username = '$user' ");
-         
-          while ($rr = $rs->fetch()) {
-                $uid=$rr['id'];
-            }
-          $r = $db->query("select * from reservation where uid= $uid");
-          $c=0;
-         while ($row = $r->fetch()) {
-                $aid[]=$row['aid'];
-                $date[]=$row['date'];
-                $time[]=$row['time'];
-                $aname[]=$row['aName'];
-                $rid[]=$row['rid'];
-                $price[]=$row['price'];
-               ++$c;
-            }
-            while ($ss = $s->fetch()) {
-                $caid[]=$ss['aid'];
-                $crid[]=$ss['rid'];
-                if(!is_null($ss['rate'])){
-                    $crate[]=100;   //if null add 100 if not add the rate value which is between  1 to 5
-                  }else $crate[]=$ss['rate']; 
-          }
-
-         
-        /*    
-         foreach($aid as $k){
-            $s= $db->query("select * from activity where id= $v");
-            while ($row = $s->fetch()) {
-                $aname[]=$row['name'];
-         }
-          
-          if($r->rowCount()>0){
-            
-          }else{$msg='no reservation yet';
-            
-          }
-*/
-
-          $db = null;
-      }
-      catch(PDOException $ex)
-      {
-          echo "Error occured";
-          die($ex->getMessage());
-      
-      }
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- My CSS -->
-    <link rel="stylesheet" href="style.css">
-    <title>Beachna</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <style>
-        .main{
-            margin-left:auto;
-            margin-right:auto;
-	       width: 80%;
-           height: auto;
-        }
-        .card-title{
-             text-align:center;
-             font-family:arial;
-             font-style:bold;
-             letter-spacing:5px;
-            
-        }
-        .card-text{
-            font-family:arial;
-             font-style:bold;
-             letter-spacing:2px;
-             font-size:20px;
-        }
-        .d p{
-          border:3px solid #17a2b9;
-          border-radius:10px 10px 10px 10px;
-          width: 70px;
-          display:inline-block;
-          color:white;
-          background:#17a2b9;
-        }
-        .d p:hover{
-          border:3px solid #17a2b9;
-          border-radius:10px 10px 10px 10px;
-          width: 70px;
-          display:inline-block;
-          background:white;
-           color:#17a2b9;
-        }
-        .d a{
-          text-decoration:none;
+  <!-- My CSS -->
+  <link rel="stylesheet" href="style.css">
+  <title>Beachna</title>
+
+  <style>
+    .main {
+      margin-left: auto;
+      margin-right: auto;
+      width: 80%;
+      height: auto;
+    }
+
+    .card-title {
+      text-align: center;
+      font-family: arial;
+      font-style: bold;
+      letter-spacing: 5px;
+
+    }
+
+    .card-text {
+      font-family: arial;
+      font-style: bold;
+      letter-spacing: 2px;
+      font-size: 20px;
+    }
+
+    .d p {
+      border: 3px solid #17a2b9;
+      border-radius: 10px 10px 10px 10px;
+      width: 70px;
+      display: inline-block;
+      color: white;
+      background: #17a2b9;
+    }
+
+    .d p:hover {
+      border: 3px solid #17a2b9;
+      border-radius: 10px 10px 10px 10px;
+      width: 70px;
+      display: inline-block;
+      background: white;
+      color: #17a2b9;
+    }
+
+    .d a {
+      text-decoration: none;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="main">
+    <?php
+    if ($c == 0) {
+      echo " <h3 class='card-title'> No Reservations Yet</h3>";
+    } else {
+      foreach ($aid as $k => $v) {
+
+        //check if the reservation passed or not //true passed 
+        $tt = 0;
+        //echo date("Y-m-j");
+        //echo $date[$k];
+        //echo date("g:ia");
+        //echo $c;
+        if ($date[$k] == date("Y-m-j")) {
+          $t = explode("-", $time[$k]);
+          $now = date("g:ia");
+
+          if (strtotime($t[1]) <= strtotime($now)) {
+            $tt++;
+
+
+          }
+
+        } else if ($date[$k] < date("Y-m-j")) {
+          $tt++;
         }
 
-        
 
-    </style>
-    </head>
-    <body>
-      <div class="main">
-        <?php
-          if($c==0){  echo " <h3 class='card-title'> No Reservations Yet</h3>";}
-          else{
-         foreach($aid as $k=>$v){
-          
-          //check if the reservation passed or not //true passed 
-            $tt=0;
-            //echo date("Y-m-j");
-            //echo $date[$k];
-            //echo date("g:ia");
-            //echo $c;
-            if ($date[$k]==date("Y-m-j")){
-                 $t=explode("-",$time[$k]);
-                 $now=date("g:ia");
-                 
-                  if(strtotime($t[1])<=strtotime($now)){
-                     $tt++;
-                     
-                     
-                  }     
-                  
-            }else if($date[$k]<date("Y-m-j")){
-                  $tt++;
-            }
 
-             
-                   
-        
-          if($tt!=0){
-            //check if the user can rate or not
-          $rt=0; //0 can rate
+
+        if ($tt != 0) {
+          //check if the user can rate or not
+          $rt = 0; //0 can rate
           //all user reservations
           //echo "t0";
-          foreach($caid as $e=>$b){ //all user comments
+          foreach ($caid as $e => $b) { //all user comments
             //echo "t00";
-              if ($v==$b){  //aid==aid.comments 
-                //echo "t1";
-                if($crid[$e]==$rid[$k]){  //rid==rid.comments
+            if ($v == $b) { //aid==aid.comments 
+              //echo "t1";
+              if ($crid[$e] == $rid[$k]) { //rid==rid.comments
                 //echo "t2";
-                  if($crate[$e]==100){ 
-                    $rt++; //user can't rate
-                    //echo "t3";
-                  }
+                if ($crate[$e] == 100) {
+                  $rt++; //user can't rate
+                  //echo "t3";
                 }
-              }    
+              }
             }
+          }
         }
-            
-          
-            ?>
-            <div class="card mt-2  text-center">
-                 <div class="card-header">
-                 <h3 class="card-title"><?php echo $aname[$k];?> </h3>
-                 </div>
-                   <div class="card-body">
-                        <p class="card-text">date:<?php echo $date[$k];?> <br>time:<?php echo $time[$k];?><br>price:<?php echo $price[$k];?>BD</p>
-                          <?php if($tt!=0){   //passed activity?> 
-                           <div class="container ">
-                            <div class="row ">
-                                <?php if($rt==0){  //user can rate  ?>
-                               <div class="col-6">
-                                    <p class="card-text">Rate Your Experience</p>
-                                    <form method='POST' action="addrate.php" > 
-                                    <input type="number" class="form-control" name="rate" id="rate" min=1 max=5>
-                                    <input type='hidden' name='aid' value='<?php echo $aid[$k];?>'/><!--hidden btn for aid -->
-                                    <input type='hidden' name='rid' value='<?php echo $rid[$k];?>'/> <!--hidden btn for rid -->
-                                    <input type="submit"  name="b1" class="btn btn-info btn-sm " value="RATE" id="b1"> 
-                                    </form>
-                                    </div>
-                                   <?php } ?>
-                                <div class="col-6 ">
-                                  <p class="card-text">Add Comment</p>
-                                  <form method='POST' action="addcomment.php" >
-                                  <input type="text" class="form-control" name="comment" id="comment">
-                                  <input type='hidden' name='aid' value='<?php echo $aid[$k];?>'/> <!--hidden btn for aid -->
-                                  <input type='hidden' name='rid' value='<?php echo $rid[$k];?>'/> <!--hidden btn for rid -->
-                                  <input type="submit" name="b2" class="btn btn-info btn-sm " value="ADD"> 
-                                  </form>
-                                  </div>
-                            <row> <!--row-->
-                          </div> <!--container-->
-                     
-                         <?php }?>
-                   </div>
-                      <div class="card-footer text-muted">
-                      <?php if($tt!=0){
-                        echo "<p style='color:green'>PREVIOUS ACTIVITY</p>"; 
-                       }else echo "<p style='color:red'>UPCOMING</p>";  ?>
-                      <a href="details.php?uid=<?php echo $uid; ?>?aid=<?php echo $aid[$k]; ?>?rid=<?php echo $rid[$k];?>">
-                      <div class="d"><p >details</p></div>
-                      </a>
-                      </div>
-            </div>
 
-        <?php } //if c ?> 
-    
-         </div>
-         <?php } //outer loop?>
-         <script>
-            function disable(){ 
-                document.getElementById('b1').disabled=true;
-            }
-        </script>
+
+        ?>
+        <div class="card mt-2  text-center">
+          <div class="card-header">
+            <h3 class="card-title"><?php echo $aname[$k]; ?> </h3>
+          </div>
+          <div class="card-body">
+            <p class="card-text">date:<?php echo $date[$k]; ?> <br>time:<?php echo $time[$k]; ?><br>price:<?php echo $price[$k]; ?>BD</p>
+            <?php if ($tt != 0) { //passed activity?>
+              <div class="container ">
+                <div class="row ">
+                  <?php if ($rt == 0) { //user can rate  ?>
+                    <div class="col-6">
+                      <p class="card-text">Rate Your Experience</p>
+                      <form method='POST' action="addrate.php">
+                        <input type="number" class="form-control" name="rate" id="rate" min=1 max=5>
+                        <input type='hidden' name='aid' value='<?php echo $aid[$k]; ?>' /><!--hidden btn for aid -->
+                        <input type='hidden' name='rid' value='<?php echo $rid[$k]; ?>' /> <!--hidden btn for rid -->
+                        <input type="submit" name="b1" class="btn btn-info btn-sm " value="RATE" id="b1">
+                      </form>
+                    </div>
+                    <?php } ?>
+                  <div class="col-6 ">
+                    <p class="card-text">Add Comment</p>
+                    <form method='POST' action="addcomment.php">
+                      <input type="text" class="form-control" name="comment" id="comment">
+                      <input type='hidden' name='aid' value='<?php echo $aid[$k]; ?>' /> <!--hidden btn for aid -->
+                      <input type='hidden' name='rid' value='<?php echo $rid[$k]; ?>' /> <!--hidden btn for rid -->
+                      <input type="submit" name="b2" class="btn btn-info btn-sm " value="ADD">
+                    </form>
+                  </div>
+                  <row> <!--row-->
+                </div> <!--container-->
+
+                <?php } ?>
+            </div>
+            <div class="card-footer text-muted">
+              <?php if ($tt != 0) {
+              echo "<p style='color:green'>PREVIOUS ACTIVITY</p>";
+            } else
+              echo "<p style='color:red'>UPCOMING</p>"; ?>
+              <a href="details.php?uid=<?php echo $uid; ?>?aid=<?php echo $aid[$k]; ?>?rid=<?php echo $rid[$k]; ?>">
+                <div class="d">
+                  <p>details</p>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <?php } //if c ?>
+
+      </div>
+      <?php } //outer loop?>
+    <script>
+      function disable() {
+        document.getElementById('b1').disabled = true;
+      }
+    </script>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+      crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
@@ -244,6 +231,6 @@ if(isset($_SESSION['admin'])){
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
-       <?php require('footer.php'); ?>
