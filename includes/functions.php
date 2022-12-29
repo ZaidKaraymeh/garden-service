075e923001833655cb70badc3a5cd1a5f8254a20
bahaa <blainefire00@gmail.com>
2022-12-29 10:46:54 +0300

    fixed pass issue

--- includes/functions.php
+++ includes/functions.php
@@ -1,86 +1,91 @@
 <?php
 
 //function to trim values
-function cleandata($value){
-    
-   return trim($value);
+function cleandata($value)
+{
+
+    return trim($value);
 }
 
 
 
 //function to sanitize value for string
-function sanitize($raw_value){                                         
-    
+function sanitize($raw_value)
+{
+
     return filter_var($raw_value, FILTER_SANITIZE_STRING);
 }
 
 
 
 //function to validate value for email
-function valemail($raw_email){
-    
+function valemail($raw_email)
+{
+
     return filter_var($raw_email, FILTER_VALIDATE_EMAIL);
-    
+
 }
 
 //function to validate value for integer
-function valint($raw_int){
-    
+function valint($raw_int)
+{
+
     return filter_var($raw_int, FILTER_VALIDATE_INT);
-    
+
 }
 
 
 //function to redirect
-function redirect($page){
-    
+function redirect($page)
+{
+
     header("Location: {$page}");
-    
+
 }
 
 
 
 //function to keep error and success messages in a session 
-function keepmsg($message){                                    
-    
-    if(empty($message)){
-        
+function keepmsg($message)
+{
+
+    if (empty($message)) {
+
         $message = "";
-    }else{
-        
-          $_SESSION['msg']    =   $message;
-        
+    } else {
+
+        $_SESSION['msg'] = $message;
+
     }
 
-    
+
 }
 
 
 //function to display the stored message in the session super global
-function showmsg(){
-    
-    if(isset($_SESSION['msg'])){
-        
+function showmsg()
+{
+
+    if (isset($_SESSION['msg'])) {
+
         echo $_SESSION['msg'];
-            
+
         unset($_SESSION['msg']);
-        
+
     }
-    
-    
+
+
 }
 
 
 
 //Create function to hash password using md5
-function hashpassword($clean_password){          
-    
-    return md5($clean_password);
-    
+function hashpassword($clean_password)
+{
+
+    return password_hash($clean_password, PASSWORD_DEFAULT);
+
 }
 
 
 ?>
-
-
-
