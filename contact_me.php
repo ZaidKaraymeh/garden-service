481ea39389312924c45d0ff2d32542b24bfb10cc
A1i-Abadi <ali0.abadi0@gmail.com>
2023-01-02 16:57:29 +0300

    Adding PHP Mailer

--- contact_me.php
+++ contact_me.php
@@ -1,5 +1,8 @@
 <?php
 // Check for empty fields
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 if (
    empty($_POST['name']) ||
    empty($_POST['email']) ||
@@ -18,6 +21,32 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 
 require('includes/config.php');
+try {
+   require 'PHPMailer-master/src/Exception.php';
+   require 'PHPMailer-master/src/PHPMailer.php';
+   require 'PHPMailer-master/src/SMTP.php';
+   $mail = new PHPMailer(true);
+   //Server settings
+   $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+   $mail->isSMTP();                                            //Send using SMTP
+   $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+   $mail->Username   = 'our-email';                     //SMTP username
+   $mail->Password   = 'pass';                               //SMTP password
+   // ciyzuvrsxdfihphs => for s2p2website@gmail.com
+   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+   $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+   //Recipients
+   $mail->setFrom($email_address,$name);
+   $mail->addAddress('our-email', "name");     //Add a recipient
+   //Content
+   $mail->Subject =  "contact us";
+   $mail->Body    =  $message;
+   $mail->send();
+} 
+catch (Exception $e) {
+   die("Email Failed");
+}
 $db = new config;
 
 $db->query("INSERT  INTO contactus VALUES (null,'$name','$message','$email_address',current_timestamp(),current_timestamp()");
