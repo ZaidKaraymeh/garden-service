a855f393b75ba770995801a135f892ddf58e1c33
ahmedinho11y <ahmedinho.y@gmail.com>
2023-01-03 11:56:49 +0300

    latest db

--- serviceSystem.sql
+++ serviceSystem.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 03, 2023 at 12:00 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Host: localhost
+-- Generation Time: Jan 03, 2023 at 09:56 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `servicesystem`
+-- Database: `serviceSystem`
 --
 
 -- --------------------------------------------------------
@@ -43,18 +43,14 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`id`, `user`, `service`, `date_time`, `period`, `payment`, `created_at`, `available`) VALUES
-(2, 787, 1, '2023-01-03', 'day', 'paypal', '2023-01-01 10:57:32', 1),
-(3, 786, 1, '2023-01-02', 'evening', 'benefit', '2023-01-01 11:22:36', 2),
-(4, 786, 2, '2023-01-23', 'day', 'paypal', '2023-01-01 13:12:43', 1),
-(5, 786, 2, '2023-02-01', 'evening', 'creditcard', '2023-01-01 13:14:57', 2),
-(6, 786, 1, '2023-01-06', 'evening', 'benefit', '2023-01-01 16:35:58', 2),
-(7, 786, 1, '2023-01-07', 'evening', 'benefit', '2023-01-01 16:36:29', 2),
-(8, 786, 1, '2023-01-04', 'evening', 'creditcard', '2023-01-01 17:25:40', 2),
-(9, 786, 1, '2023-01-04', 'day', 'paypal', '2023-01-01 17:25:57', 1),
-(10, 786, 3, '2023-01-11', 'day', 'benefit', '2023-01-02 09:38:42', 1),
-(11, 786, 1, '2023-01-02', 'day', 'benefit', '2023-01-02 21:17:57', 1),
-(12, 789, 3, '2023-01-02', 'evening', 'debit', '2023-01-02 21:18:37', 2),
-(13, 786, 1, '2023-01-05', 'evening', 'debit', '2023-01-03 01:18:17', 2);
+(15, 789, 1, '2023-01-02', 'day', 'paypal', '2023-01-01 22:00:27', 1),
+(16, 789, 1, '2023-01-03', 'evening', 'paypal', '2023-01-02 13:07:09', 2),
+(17, 789, 1, '2023-01-03', 'day', 'benefit', '2023-01-02 19:25:08', 1),
+(18, 789, 1, '2023-01-05', 'evening', 'debit', '2023-01-02 20:47:22', 2),
+(19, 790, 1, '2023-01-12', 'day', 'benefit', '2023-01-03 10:05:24', 1),
+(20, 790, 1, '2023-01-13', 'day', 'benefit', '2023-01-03 10:08:28', 1),
+(21, 790, 1, '2023-01-05', 'day', 'benefit', '2023-01-03 10:10:36', 1),
+(22, 790, 1, '2023-01-15', 'day', 'creditcard', '2023-01-03 10:15:49', 1);
 
 -- --------------------------------------------------------
 
@@ -67,7 +63,6 @@ CREATE TABLE `comment` (
   `user` int(11) NOT NULL,
   `service` int(11) NOT NULL,
   `comment` text NOT NULL,
-  `comment_type` char(8) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -76,13 +71,16 @@ CREATE TABLE `comment` (
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`id`, `user`, `service`, `comment`, `comment_type`, `created_at`, `updated_at`) VALUES
-(12, 786, 1, 'hello', '', '2023-01-03 01:35:01', '2023-01-03 01:35:01'),
-(13, 786, 1, 'thanks', '', '2023-01-03 01:35:11', '2023-01-03 01:35:11'),
-(14, 786, 1, 'thanks', '', '2023-01-03 01:38:24', '2023-01-03 01:38:24'),
-(15, 786, 1, 'thanks', '', '2023-01-03 01:38:32', '2023-01-03 01:38:32'),
-(16, 786, 1, 'thanks', '', '2023-01-03 01:38:46', '2023-01-03 01:38:46'),
-(17, 786, 1, 'hi', '', '2023-01-03 01:40:50', '2023-01-03 01:40:50');
+INSERT INTO `comment` (`id`, `user`, `service`, `comment`, `created_at`, `updated_at`) VALUES
+(11, 789, 1, 'Very good ', '2023-01-01 22:01:07', '2023-01-01 22:01:07'),
+(14, 789, 1, 'Pretty good customer service', '2023-01-02 13:08:04', '2023-01-02 13:08:04'),
+(15, 789, 1, 'I like it!', '2023-01-02 19:55:47', '2023-01-02 19:55:47'),
+(16, 789, 1, 'Not that good', '2023-01-02 20:02:58', '2023-01-02 20:02:58'),
+(17, 789, 1, 'WORTHY SERVICE', '2023-01-02 20:37:11', '2023-01-02 20:37:11'),
+(18, 790, 1, 'VVGOOD', '2023-01-03 10:05:38', '2023-01-03 10:05:38'),
+(19, 790, 1, 'ABCD', '2023-01-03 10:08:48', '2023-01-03 10:08:48'),
+(20, 790, 1, 'YES', '2023-01-03 10:10:48', '2023-01-03 10:10:48'),
+(21, 790, 1, 'vgood', '2023-01-03 10:46:58', '2023-01-03 11:55:01');
 
 -- --------------------------------------------------------
 
@@ -132,12 +130,13 @@ CREATE TABLE `review` (
 --
 
 INSERT INTO `review` (`id`, `user`, `service`, `rating`, `created_at`, `updated_at`) VALUES
-(3, 786, 1, '4', '2023-01-03 01:35:01', '2023-01-03 01:35:01'),
-(4, 786, 1, '4', '2023-01-03 01:35:11', '2023-01-03 01:35:11'),
-(5, 786, 1, '4', '2023-01-03 01:38:24', '2023-01-03 01:38:24'),
-(6, 786, 1, '4', '2023-01-03 01:38:32', '2023-01-03 01:38:32'),
-(7, 786, 1, '4', '2023-01-03 01:38:46', '2023-01-03 01:38:46'),
-(8, 786, 1, '5', '2023-01-03 01:40:50', '2023-01-03 01:40:50');
+(3, 789, 1, '5', '2023-01-01 22:01:04', '2023-01-01 22:01:04'),
+(4, 789, 1, '4', '2023-01-02 13:08:04', '2023-01-02 13:08:04'),
+(5, 789, 1, '4', '2023-01-02 19:55:47', '2023-01-02 19:55:47'),
+(6, 790, 1, '4', '2023-01-03 10:05:38', '2023-01-03 10:05:38'),
+(7, 790, 1, '4', '2023-01-03 10:08:48', '2023-01-03 10:08:48'),
+(8, 790, 1, '5', '2023-01-03 10:10:48', '2023-01-03 10:10:48'),
+(9, 790, 1, '5', '2023-01-03 10:46:58', '2023-01-03 10:46:58');
 
 -- --------------------------------------------------------
 
@@ -161,10 +160,11 @@ CREATE TABLE `service` (
 --
 
 INSERT INTO `service` (`id`, `name`, `price`, `picture`, `description`, `rating`, `created_at`, `updated_at`) VALUES
-(1, 'Ex', 300, 'service-1672479209144234523763b001e988f83.jpg', 'ijdeheuefuhfeuefugfufeueudfdnjjnsdbhdbhddbh', 0, '2022-12-31 12:33:29', '2022-12-31 13:25:25'),
-(2, 'Testing', 500, 'service-1672482573101473983563b00f0d47cab.jpg', 'Hello', 0, '2022-12-31 13:29:33', '2022-12-31 13:29:33'),
-(3, 'Hello', 1000, 'service-1672639596170212322163b2746c63980.jpg', 'jewbjewvdevdshsvhdsdhc', 0, '2023-01-02 09:06:36', '2023-01-02 09:06:36'),
-(4, 'jsanjsdbf', 200, 'service-167264155959269523963b27c17d6ff9.png', 'jdsndsnujdu', 0, '2023-01-02 09:39:19', '2023-01-02 09:39:19');
+(1, 'Excavation', 300, 'service-1.jpg', 'The cost to remove a palm tree does depend on a few factors. Size is one of them, but also the palm species and it\'s location on your proerty.', 0, '2022-12-31 12:33:29', '2023-01-01 21:53:00'),
+(2, 'Landscape Design', 500, 'service-2.jpg', 'We provide various types of exotic flowers, fruit trees, palms and expert knowledge to help you decide what is best.', 0, '2022-12-31 13:29:33', '2023-01-01 21:54:34'),
+(3, 'Installation of irrigation systems', 350, 'service-3.jpg', 'A sprinkler system keep your grass, plants, and trees healthy–if properly set up and maintained, it can also help conserve water. Having a good system can also boost the resale value of your home or building.', 0, '2023-01-01 21:55:27', '2023-01-01 21:55:27'),
+(4, 'Edging, Trimming and Shifting', 400, 'service-4.png', 'With out expertise gardeners we\'ll provide you with all the care your garden needs wheater you\'re planting new trees, lawn mowers etc.', 0, '2023-01-01 21:56:11', '2023-01-01 21:56:11'),
+(5, 'Cleaning', 100, 'service-1672729462130870853163b3d376eb940.jpeg', '12345', 0, '2023-01-03 10:04:22', '2023-01-03 10:04:22');
 
 -- --------------------------------------------------------
 
@@ -191,9 +191,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `fullName`, `image`, `email`, `password`, `phone_number`, `user_type`, `created_at`, `updated_at`, `spending`) VALUES
 (785, 'Ali Abadi', '1.png', 'aasd@ss.com', '$2y$10$s7W41j7/chNnHHGgDe9lkO2rWEsgu3/3wWpD3s74LppFGl5q11xWW', '33331362', 'CTM', '2022-12-24 23:30:36', '2022-12-30 10:35:01', 0),
-(786, 'Admin', '1.png', 'admin@admin.com', '$2y$10$s7W41j7/chNnHHGgDe9lkO2rWEsgu3/3wWpD3s74LppFGl5q11xWW', '33123456', 'Adm', '2022-12-29 11:41:49', '2023-01-03 01:18:17', 3830),
-(787, 'ali', 'user_default.jpg', 'ali@ali.com', '$2y$10$4vnQfEVt8ekdAt.N/WJyxO4kAJ0g/BBbPNZy.I5LTmg2REccfdJSK', '33345678', 'CTM', '2022-12-30 10:57:02', '2023-01-02 19:23:26', 300),
-(789, 'ali ali', 'user_default.jpg', 'somb@somb.com', '$2y$10$QUEO8PP/Yd3TtIyrswROQuLHWEkIL8Um7paXV97lKX5kn6HU5SAXG', '33345678', 'CTM', '2023-01-02 09:43:39', '2023-01-02 09:43:39', 0);
+(786, 'Admin', '1.png', 'admin@admin.com', '$2y$10$s7W41j7/chNnHHGgDe9lkO2rWEsgu3/3wWpD3s74LppFGl5q11xWW', '33123456', 'Adm', '2022-12-29 11:41:49', '2022-12-30 10:35:05', 0),
+(787, 'ali', 'user_default.jpg', 'ali@ali.com', '$2y$10$4vnQfEVt8ekdAt.N/WJyxO4kAJ0g/BBbPNZy.I5LTmg2REccfdJSK', '33345678', 'CTM', '2022-12-30 10:57:02', '2022-12-31 16:08:49', 0),
+(789, 'Ahmed  Yusuf', 'user_default.jpg', 'ahmed@gmail.com', '$2y$10$ZEG5YVmTc0BvFHe3QJeKruIwML//aNa0yi89mjgHhhiZNZ6.BobbC', '36728829', 'CTM', '2023-01-01 18:39:59', '2023-01-03 10:23:21', 0),
+(790, 'Ahmed  Yusuf', 'user_default.jpg', 'admin@ahmed.com', '$2y$10$i18A1lHn1PQ4rBHsyCi8yeG.WIpAhi3Cd.vCxyPEyeN2MqNYgHOly', '36728829', 'Adm', '2023-01-03 09:59:50', '2023-01-03 10:15:49', 1320);
 
 --
 -- Indexes for dumped tables
@@ -258,13 +259,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `rating_table`
@@ -276,19 +277,19 @@ ALTER TABLE `rating_table`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=790;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=791;
 
 --
 -- Constraints for dumped tables
